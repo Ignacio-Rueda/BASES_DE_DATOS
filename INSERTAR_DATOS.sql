6406f1b906823e9015084e84458c1eaaf12074b7
Ignacio Rueda <nachord@outlook.com>
2024-08-24 19:35:00 +0200

    Insertar datos

---
+++ INSERTAR_DATOS.sql
@@ -0,0 +1,11 @@
+INSERT INTO propietario (nombre,telefono,nacion)
+	VALUES ('Alberto Crespo','679346678','España'),
+		   ('Mónica Paredes','661294715','Italia'),
+		   ('Eva Jimena','603682187','España');
+
+
+INSERT INTO empleado (nombre,email,fechaini)
+	VALUES('Luis Romero','luis@gmail.com','2016-01-02'),
+		  ('Ana Sánchez','ana@gmail.com','2017-07-01'),
+		  ('Lucía Parrilla','lucia@gmail.com','2018-08-15');
+		  
